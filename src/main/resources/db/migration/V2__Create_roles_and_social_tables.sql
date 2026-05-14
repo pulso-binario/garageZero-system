a046763511db935b69fd43c065b68260749e29e1
Augusto H. <augusto.hernandez@tfja.edu.mx>
2026-05-13 18:41:06 -0600

    aHernandez : 0.0.3-SNAPSHOT
    Se elimina dependencia duplicada de flyway.
    Se cambia VARCHAR2 por VARCHAR.

--- src/main/resources/db/migration/V2__Create_roles_and_social_tables.sql
+++ src/main/resources/db/migration/V2__Create_roles_and_social_tables.sql
@@ -1,8 +1,9 @@
+-- TODO: Cambiar regla de Sonarquube para SQL -> POSTGRES.
 -- 1. Roles table.
 CREATE TABLE roles
 (
     id   SERIAL PRIMARY KEY,
-    name VARCHAR2(50) UNIQUE NOT NULL
+    name VARCHAR(50) UNIQUE NOT NULL
 );
 
 -- 2. User-Roles Junction Table (Many-to-many)
@@ -17,8 +18,8 @@ CREATE TABLE user_roles
 CREATE TABLE social_accounts(
     id UUID PRIMARY KEY,
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    provider VARCHAR2(50) NOT NULL, -- e.g., 'google', 'apple'.
-    provider_id VARCHAR2(255) NOT NULL, -- The unique ID returned by Google/Apple.
+    provider VARCHAR(50) NOT NULL, -- e.g., 'google', 'apple'.
+    provider_id VARCHAR(255) NOT NULL, -- The unique ID returned by Google/Apple.
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     UNIQUE (provider, provider_id) -- Ensures a specific Google account can only be linked once.
 );
